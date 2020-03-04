e1dfa8e55578d4bee9790f954262c49074806b8b
Aaron Thompson <aaron.thompson1889@outlook.com>
2020-03-04 12:45:25 -0600

    Work on memviolator
    This is the assignment that should test memory violations in xv6, and the c file itself should be able to run in linux as well.

--- memviolator.c
+++ memviolator.c
@@ -6,8 +6,53 @@
 #include "stat.h"
 #include "user.h"
 #endif
+//if "if def __pie__" is defined, you are in linux world
+//-----GLOBAL VARIABLES-----
+char * p;
+int size;
+
+//-----MENU FUNCTIONS-----
+/*
+void mainMenu()
+{
+    char * mainmenustring = "What would you like to do?\n(1) Read \n(2) Write\n(3) Exit";
+    int n;
+    #if def __pie__
+        print(mainmenustring);
+    #else
+        printf(1,mainmenustring);
+        char * integerthing;
+        read(0,integerthing, 4);
+        n = atoi(integerthing);
+    #endif
+    switch(n)
+    {
+
+    }
+}
+*/
+//-----MEMORY FUNCTIONS-----
+void memoryViolatorR()
+{
+    int memoryData;
+    #if def __pie__
+        scanf
+    #else
+        read(*p, memoryData, size);
+
+    #endif
+}
+
+void memoryViolatorW()
+{
+
+}
 
 void main(int argc, char * argv)
 {
+    * p = atoi(argv[1]);
+    size = atoi(argv[2]);
     
+    //while(1)
+    //    mainMenu();
 }
